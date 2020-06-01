13a795fcd339c7ac6ab3668f59878b52caf68a71
Jesse <jzw0169@auburn.edu>
2020-06-01 18:37:38 +0000

    Lots of db logic

--- src/main/java/edu/au/cc/gallery/UI.java
+++ src/main/java/edu/au/cc/gallery/UI.java
@@ -5,12 +5,42 @@ import java.util.Scanner;
 public class UI {
 
 	public static void main(String[] args) {
+
+		mainMenu();
+	}
 	
+	public static void mainMenu() {
+
+		int response;
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("1) List Users");
 		System.out.println("2) Add User");
 		System.out.println("3) Edit Users");
 		System.out.println("4) Delete User");
 		System.out.println("5) Quit");
+		System.out.println("Enter Command>");
+		response = sc.nextInt();
+
+		switch (response) {
+			case 1: UserAdmin.listUsers();
+				mainMenu();
+				break;
+			case 2: UserAdmin.addUser();
+				mainMenu();
+				break;
+			case 3: UserAdmin.editUser();
+				mainMenu();
+				break;
+			case 4: UserAdmin.deleteUser();
+				mainMenu();
+				break;
+			case 5: System.out.println("Bye.");
+				System.exit(1);
+				break;
+			default : System.out.println("Invalid Selection.");
+				  mainMenu();
+		}
 	}
 	
 }
--- src/main/java/edu/au/cc/gallery/tools/UserAdmin.java
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin.java
@@ -1,44 +1,125 @@
 package edu.au.cc.gallery;
 
 import java.util.Scanner;
+import java.sql.ResultSet;
 
 public class UserAdmin {
 
 
-	public addUser() {
+	public static void addUser() {
 		
-		private String username;
-		private String password;
-		private String fullName;
-		private String response;
+		String username;
+		String password;
+		String fullName;
+		String response;
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter username: ");
-		username = sc.next();
+		username = sc.nextLine();
 
 		System.out.println("Enter password: ");
-		password = sc.next();
+		password = sc.nextLine();
 
 		System.out.println("Enger Full Name: ");
 		fullName = sc.nextLine();
 
 		System.out.println("Adding user: " + username + " with password: " + password 
-				+ " and full name: " +fullName;
-		System.out.println("Is this correct? (Y/N)";
-		response = sc.next();
+				+ " and full name: " +fullName);
+		System.out.println("Is this correct? (Y/N)");
+		response = sc.nextLine();
+		
+		if (response == "Y" || response == "y") {
+			DB db = new DB();
+			db.connect();
+			db.execute("insert into users (username, password, full_name) values ("+username+", "+password+", "+fullName")");
+			db.close();
+		} else {
+			addUser();
+		}
+
+	}
+
+	public static void listUsers() {
+
+		ResultSet rs = new ResultSet();
+		DB db = new DB();
+		db.connect();
+		rs = db.execute("select * from users");
+		System.out.println("username\tpassword\t\full name");
+		System.out.println("------------------------------");
+		while (rs.next()) {
+			System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3));
+		}
+	}
+
+	public static void editUser() {
+		
+		String username;
+		String password;
+		String fullName;
+		String response;
+		Scanner sc = new Scanner(System.in);
+		ResultSet rs = new ResultSet();
+
+		System.out.println("Enter username to edit: ");
+		username = sc.nextLine();
+
+		DB db = new DB();
+		db.connect();
+		rs = db.execute("select * from users where username='"+username+"'");
+		if (username != rs.getString(1)) {
+			db.close();
+			System.out.println("That user doesn't exist.");
+			break;
+		} else {
+			System.out.println("New password (press enter to keep current): ");
+			password = sc.nextLine();
+			System.out.println("New full name (press enter to keep current): ");
+			fullName = sc.nextLine();
+
+			if (password == '' & fullName == '') {
+
+				System.out.println("No updates needed.");
+
+			}
+			else if (password == '') {
+
+				db.execute("update users set full_name='"+fullName+"' where username = '"+username"'");
+				db.close();
+			}
+			else if (fullName == '') {
+
+				db.execute("update users set password='"+password+"' where username = '"+username"'");
+				db.close();
+			} else {
+
+				db.execute("update users set password='"+password+"', full_name='"+fullName+"' where username = '"+username"'");
+				db.close();
+			}
 
 
 	}
 
-	public listUsers() {
-		//TODO
-	}
+	public static void deleteUser() {
+		String username;
+		String response;
+		Scanner sc = new Scanner(System.in);
 
-	public editUser() {
-		//TODO
-	}
+		System.out.println("Enter a username to delete: ");
+		username = sc.nextLine();
 
-	public deleteUser() {
-		//TODO
+		System.out.println("Are you sure you want to delete " + username + "? (Y/N)");
+		response = sc.nextLine();
+
+		if (response == "Y" || response == "y") {
+			DB db = new DB();
+			db.connect();
+			db.execute("delete from users where username='"+username+"'");
+			db.close();
+
+			System.out.println("Deleted.");
+		} else {
+			System.out.println("Aborted.);
+		}
 	}
 }
