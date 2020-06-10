b81304d39f61ca1d714371f07b6b5d47705d9fcd
Jesse <jzw0169@auburn.edu>
2020-06-10 17:46:26 +0000

    quick fix

--- src/main/java/edu/au/cc/gallery/Secret.java
+++ src/main/java/edu/au/cc/gallery/Secret.java
@@ -1,19 +1,32 @@
-public static void getSecret() {
+package edu.au.cc.gallery;
+
+import software.amazon.awssdk.regions.Region
+
+import software.amazon.awssdk.services.secretsmanager.*;
+import software.amazon.awssdk.services.secretsmanager.model.*;
+
+import java.util.Base64;
+
+public class Secret {
+
+public static String getSecret() {
 
     String secretName = "sec-ig-image_gallery";
     String region = "us-east-2";
 
     // Create a Secrets Manager client
     AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard()
-                                    .withRegion(region)
-                                    .build();
+                                   // .withRegion(region)
+            .region(region)                        
+	    .build();
     
 
     
     String secret, decodedBinarySecret;
-    GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
-                    .withSecretId(secretName);
-    GetSecretValueResult getSecretValueResult = null;
+    GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().builder()
+                    .secretId(secretName)
+		    .build();
+    GetSecretValueResponse getSecretValueResult = null;
 
     try {
         getSecretValueResult = client.getSecretValue(getSecretValueRequest);
@@ -39,11 +52,7 @@ public static void getSecret() {
         throw e;
     }
 
-    if (getSecretValueResult.getSecretString() != null) {
-        secret = getSecretValueResult.getSecretString();
-    }
-    else {
-        decodedBinarySecret = new String(Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
+    return getSecretValueResult.secretString();
     }
 
     // Your code goes here.
