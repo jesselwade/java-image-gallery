51e181cfae19698ccf0c0a266ec44c6944c5f177
Jesse <jzw0169@auburn.edu>
2020-06-22 13:22:40 +0000

    script updates

--- ec2-scripts/ec2-nginx-1.0.sh
+++ ec2-scripts/ec2-nginx-1.0.sh
@@ -13,12 +13,6 @@ CONFIG_BUCKET="s3://edu.au.cc.jzw.image-gallery-config"
 aws s3 cp ${CONFIG_BUCKET}/nginx/nginx.conf /etc/nginx/nginx.conf
 aws s3 cp ${CONFIG_BUCKET}/nginx/default.d/image_gallery.conf /etc/nginx/default.d/image_gallery.conf
 
-# Start/enable services
-systemctl stop postfix
-systemctl disable postfix
-systemctl start nginx
-systemctl enable nginx
-
 su ec2-user -l -c 'curl -s "https://get.sdkman.io" | bash && source .bashrc && sdk install gradle'
 
 # Config and install source code
@@ -27,6 +21,10 @@ git clone https://github.com/jesselwade/java-image-gallery.git
 chown -R ec2-user:ec2-user java-image-gallery
 
 # Start/enable services
+systemctl stop postfix
+systemctl disable postfix
+systemctl start nginx
+systemctl enable nginx
 
 su ec2-user -l -c 'cd ~/java-image-gallery && ./start' >/var/log/image_gallery.log 2>&1 &
 
