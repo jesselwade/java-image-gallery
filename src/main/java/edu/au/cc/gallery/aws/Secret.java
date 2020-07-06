25a7cc42ce183d2136a3284dd2ea1f759bbfd472
EC2 Default User <ec2-user@ip-172-31-46-32.us-east-2.compute.internal>
2020-07-06 15:51:53 +0000

    new secret for cognito m7

--- src/main/java/edu/au/cc/gallery/aws/Secret.java
+++ src/main/java/edu/au/cc/gallery/aws/Secret.java
@@ -9,6 +9,44 @@ import java.util.Base64;
 
 public class Secret {
 
+public static String getSecretCognito() {
+
+    String secretName = "sec-ig-cognito";
+    Region region = Region.US_EAST_2;
+
+    // Create a Secrets Manager client
+    SecretsManagerClient client  = SecretsManagerClient.builder().region(region).build();
+
+
+
+    String secret, decodedBinarySecret;
+    GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+            .secretId(secretName)
+            .build();
+    GetSecretValueResponse getSecretValueResult = null;
+
+    try {
+        getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+    } catch (DecryptionFailureException e) {
+        // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    } catch (InternalServiceErrorException e) {
+        // An error occurred on the server side.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    } catch (InvalidParameterException e) {
+        // You provided an invalid value for a parameter.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    } catch (InvalidRequestException e) {
+        // You provided a parameter value that is not valid for the current state of the resource.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    } catch (ResourceNotFoundException e) {
+        // We can't find the resource that you asked for.
+
+
 public static String getSecret() {
 
     String secretName = "sec-ig-image_gallery";
